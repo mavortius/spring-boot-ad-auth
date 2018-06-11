9917a90c9bdf2ad77c8657321bfa7c6d01097c9e
Marcelo Martins <m.a.martins@lve.com>
2018-06-11 17:10:08 -0300

    Changed message

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.xml.ws.Response;
 import java.util.Optional;
 
 @RestController
@@ -23,10 +26,20 @@ public class HomeController {
     }
 
     @GetMapping
-    public ResponseEntity<UserProfile> home() {
-        return Optional.ofNullable(profileService.getUserProfile())
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<String> home() {
+        UserProfile user = profileService.getUserProfile();
+        String hello = "Olá, " + user.getGivenName() + ", seu email é " + user.getUserPrincipalName();
+
+        return ResponseEntity.ok(hello);
+    }
+
+    @GetMapping("/microsoft-logout")
+    public ResponseEntity<String> logout(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+
+        session.invalidate();
+
+        return ResponseEntity.ok("Você saiu");
     }
 
 }
